301f2d5d3873e808f8b9ec5b9d59e49a7032ea30
Devin Don <I.INF@Outlook.com>
2020-04-24 20:19:49 +0800

    feat(demo): whatever, it works

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,3 +1,22 @@
-import { hello } from '../main';
+import { Aria2 } from '../main';
 
-console.log(hello('TypeScript template'));
+async function main() {
+
+  const aria2 = new Aria2({
+    host: '192.168.0.241',
+    port: 6800,
+    secure: false,
+    path: '/jsonrpc',
+    secret: 'SECRET'
+  });
+
+  await aria2.connect();
+  // const response = await aria2.tellActive();
+  const uri = 'https://storage.don.red/download/ubuntu-20.04-live-server-amd64.iso?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=Bscefq2J99vuUjZz%2F20200424%2F%2Fs3%2Faws4_request&X-Amz-Date=20200424T115117Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&X-Amz-Signature=274b09035b5a490941a01fd58adafc0e5bca442f094c182896237cadd498feab';
+  const response = await aria2.addUri([uri]);
+  console.log('Response:', response);
+  // aria2.disconnect();
+
+}
+
+main();
