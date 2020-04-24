91fcf641d25c0210aa400e347dd7e9970b83293b
Devin Don <I.INF@Outlook.com>
2020-04-24 21:04:35 +0800

    test(index): add before each

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1,6 +1,19 @@
-import { Pending } from '../main';
+import { Aria2, Option } from '../main';
 
 describe('Hello, world!', () => {
+
+  let aria2: Aria2;
+
+  beforeAll(done => {
+    const option: Option = {
+      host: 'localhost',
+      port: 6800,
+      secure: false,
+      path: '/jsonrpc'
+    };
+    aria2 = new Aria2(option);
+    done();
+  });
 
   // it('should say `Hello, world!`', done => {
   //   expect(hello('world')).toEqual('Hello, world!');
